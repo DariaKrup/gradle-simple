a826f1d846b89b409ee24325fbb52830566d2026
dkrupkina <dariakrupkina@gmail.com>
2024-07-25 11:20:21 +0200

    Revert "Refactor tests"
    This reverts commit 8458570759f797d99a42815cfea9be06910e067c.

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -3,11 +3,12 @@ import static org.junit.Assert.*;
 
 
 public class SimpleTest {
+
     @Test
     public void testConcatenate() {
-        mathService = new MathService();
-        int result = mathService.multiply(2, 4);
+        String result = "one";
+        result = result.concat("two");
 
-        assertThat(result, equalTo(8));
+        assertEquals("onetwo", result);
     }
 }
